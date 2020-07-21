f60c6e563dadd9e3be9c2400c917ce7d5de894ae
Your Name <you@example.com>
2020-07-21 21:21:15 +0300

    init data with all sub strings ignore bad characters

--- Google_project.py
+++ Google_project.py
@@ -1,65 +1,26 @@
+from itertools import combinations
+from collections import defaultdict
 
 sent1 = "Hello big and beautiful world "
-sent2 = "To be or not to be, This is the question and"
+sent2 = "To be or not to be, This is the question"
 sent3 = "Be good to everyone, everywhere, everytime"
 
-sent_dict = [sent1, sent2, sent3]
-bad_chars = [';', ':', '!', "*", ",", "$", "@"]
+sent_dict = {sent1: 0, sent2: 1, sent3: 2}
+bad_chars = [';', ':', '!', '*', ',', '$', "@" " "]
 
-data = {}
-
-
-# for sentence in sent_dict:
-#     new_sentence = sentence.split()
-#     # set_sent = set(new_sentence)
-#     for word in new_sentence:
-#         for i in bad_chars:
-#             word = word.replace(i, '')
-#         if word not in data:
-#             # data[word.lower()] = sentence
-#             # set_sent = set(sentence)
-#             data[word.lower()] = sentence
-#             # set_sent.add(data[word.lower()])
-#         else:
-#             # if data[word.lower()] != sentence:
-#                 # data[word.lower()] += ", "
-#                 # data[word.lower()] += sentence
-#             # if data[word.lower()] not in set_sent:
-#             # a = data[word.lower()] + sentence
-#             # print(f'aaaa{a}')
-#             # print(type(a))
-#             print(type(data[word.lower()]))
-#             print(type(sentence))
-#
-#             # d = set(a)
-#             data[word.lower()] = set(list(data[word.lower()]).append(sentence))
-#             print(type(data[word.lower()]))
-#
-#                 # set_sent.add(data[word.lower()])
-# for sentence in sent_dict:
-#     new_sentence = sentence.split()
-#     for word in new_sentence:
-#         for i in bad_chars:
-#             word = word.replace(i, '')
-#         if word not in data:
-#             data[word.lower()] = sentence
-#         else:
-#             if data[word.lower()] != sentence:
-#                 data[word.lower()] = data[word.lower()] + ", " + sentence
+data = defaultdict(set)
 
 
 for sentence in sent_dict:
-    for i in range(len(sentence)):
-        shortSen = sentence[:i]
-        if not shortSen.endswith(" "):
-            data[shortSen] = sentence
+    substr_list = [sentence[x:y] for x, y in combinations(range(len(sentence) + 1), r=2)]
+    for substr in substr_list:
+        for char in bad_chars:
+            substr = substr.replace(char, '')
+        if not substr.startswith(" ") and not substr.endswith(" "):
+            data[substr].add(sent_dict[sentence])
 
-
-# word = "hello"
-# if word in data:
-#     print(data[word])
 
 for item in data:
     print(f'{item}: {data[item]}')
 
-# print(data)
+
