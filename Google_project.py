e652ed1970d170917222b3dabfcfae343b13ada0
Your Name <you@example.com>
2020-07-23 01:52:14 +0300

    add input from user

--- Google_project.py
+++ Google_project.py
@@ -1,6 +1,9 @@
+import string
 from itertools import combinations
 from collections import defaultdict
 from string import ascii_lowercase
+import glob
+
 
 sent1 = "Hello big and beautiful world "
 sent2 = "To be or not to be, This is the question"
@@ -9,33 +12,42 @@ sent4 = "Good, better, best, never let them rest, Make the good better and the b
 sent5 = "Bee is making honey"
 sent6 = "We are learning in Beit - Yaakov"
 
-sent_dict = {sent1: 0, sent2: 1, sent3: 2, sent4: 3, sent5: 4, sent6: 5}
-bad_chars = [';', ':', '!', '*', ',', '$', "@", " "]
+sentences_dict = {sent1: 0, sent2: 1, sent3: 2, sent4: 3, sent5: 4, sent6: 5}
+bad_chars = [';', ':', '!', '*', ',', '$', '@', '-']
 best_sentences = {}
 data = defaultdict(set)
 
 
-def init_data():
-    for sentence in sent_dict:
+def init_data(sentences_dict):
+    for sentence in sentences_dict.keys():
         substr_list = [sentence[x:y] for x, y in combinations(range(len(sentence) + 1), r=2)]
         for substr in substr_list:
-            # substr = substr.lower()
             for char in bad_chars:
                 substr = substr.replace(char, '')
             if not substr.startswith(" ") and not substr.endswith(" "):
-                data[substr.lower()].add(sent_dict[sentence])
+                data[substr.lower()].add(sentences_dict[sentence])
 
 
-def print_data():
+def print_data(data):
     for item in data:
         print(f'{item}: {data[item]}')
 
 
-# def read_file():
-#     file = open("demofile.txt", "r")
-#     line = file.readline()
-#
-#     file.close()
+def read_data(file_name):
+    data_file = open(file_name, encoding="utf8")
+    data_sentences = data_file.read().split("\n")
+    my_dict = {}
+    for sent in data_sentences:
+        my_dict[sent] = file_name
+    return my_dict
+
+
+    # txt_files = "C:\Users\RENT\Documents\project_Google"
+    # for file in glob.glob(f"{txt_files}/*.txt"):
+    #     self.sentence_data += self.data_from_file(file)
+    #     with open(file, encoding="utf8") as f:
+    #         sentences = f.readlines()
+    #     return [x.translate(str.maketrans('', '', '\n' + string.punctuation)) for x in sentences]
 
 
 def get_best_k_completions(prefix):
@@ -50,7 +62,7 @@ def get_data_at_key(key):
     key = key.lower()
     sentences = []
     for k in data[key]:
-        sentences.append(list(sent_dict.keys())[k])
+        sentences.append(list(sentences_dict.keys())[k])
     return sorted(sentences)
 
 
@@ -61,8 +73,8 @@ def get_five_best_sentences(prefix):
     else:
         for i in replace_char(prefix)[0]:
             best_sentences[i] = replace_char(prefix)[1]
-        for i in delete_Unnecessary_char(prefix)[0]:
-            best_sentences[i] = delete_Unnecessary_char(prefix)[1]
+        for i in delete_unnecessary_char(prefix)[0]:
+            best_sentences[i] = delete_unnecessary_char(prefix)[1]
         for i in add_missed_char(prefix)[0]:
             best_sentences[i] = add_missed_char(prefix)[1]
         a = set(sorted(best_sentences, key=best_sentences.get, reverse=True))
@@ -70,20 +82,8 @@ def get_five_best_sentences(prefix):
         return list(a)[:5]
 
 
-#  def get_sentence_score(sentence):
-#
-#
-# # return sentence with high score
-# def high_score_sentence(key):
-#     result = get_data_at_key(key)
-#     max_score = 0
-#     sent_index = 0
-#     for index, sentence in enumerate(result):
-#         score = get_sentence_score(sentence)
-#         if score > max_score:
-#             max_score = score
-#             sent_index = index
-#     return result[sent_index]
+# def get_sentence_score(sentence):
+    # return sentences_dict[sentence]
 
 
 def replace_char(word):
@@ -100,12 +100,12 @@ def replace_char(word):
     return [], 0
 
 
-def delete_Unnecessary_char(word):
+def delete_unnecessary_char(word):
     for index, char in enumerate(word):
         if word.replace(char, "") in data.keys():
             score = 0
             if index < 5:
-                score -= 10 - index % 5
+                score -= (10 - index % 5)
             else:
                 score -= 2
             score += (len(word) - 1) * 2
@@ -119,10 +119,10 @@ def add_missed_char(word):
             if word.replace(char, char + i) in data.keys():
                 score = 0
                 if index < 5:
-                    score -= 10 - index % 5
+                    score -= (10 - index % 5)
                 else:
                     score -= 2
-                score += (len(word) - 1) * 2
+                score += (len(word) * 2)
     return [], 0
 
 
@@ -130,8 +130,8 @@ class AutoCompleteData:
     def __init__(self, text_input):
         self.completed_sentence = text_input
         self.source_text = ""
-        self.offset = 0
-        self.score = get_five_best_sentences(text_input)[0]
+        self.offset = sentences_dict[text_input]
+        self.score = get_five_best_sentences(text_input)
 
     def get_completed_sentence(self):
         return self.completed_sentence
@@ -147,23 +147,21 @@ class AutoCompleteData:
 
 
 # Tests:
-init_data()
-# print_data()
-# print(get_Data_at_key("to"))
-# acd = AutoCompleteData(text)
-# print(acd.get_completed_sentence("to"))
-# print(acd.replace_char("kello"))
-# print(acd.delete_Unnecessary_char("helplo"))
-# print(acd.add_missed_char("helo"))
-# print(acd.get_score("be"))
+# init_data()
 
-print(get_five_best_sentences("everyote"))
+if __name__ == '__main__':
+    print("Loading the files and preparing the system...")
+    init_data()
+    text = input("The system is ready, Enter your text: ")
+    while text != '#':
+        result = get_best_k_completions(text)
+        i = len(result)
+        print(f"There are {i} suggestions:")
+        for index, item in enumerate(result):
+            print(f'{index + 1}. {item.get_completed_sentence()} ({item.get_source_text()} {item.get_offset()})')
+        print(text)
+        text = input()
 
-# if __name__ == '__main__':
-#     print("Loading the files and preparing the system...")
-#     init_data()
-#     text = input("The system is ready, Enter your text: ")
-#     result = get_best_k_completions(text)
-#     for item in result:
-#         print(item.get_completed_sentence())
-#     print("There are suggestions")
+t = read_data("data.txt")
+# print_data(t)
+print_data(data)
