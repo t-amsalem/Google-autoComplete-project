b924e87a2eae52afd88d8838446ee9c356b09c98
Your Name <you@example.com>
2020-07-23 05:06:20 +0300

    clean the code, deleting comments etc

--- Google_project.py
+++ Google_project.py
@@ -2,7 +2,7 @@ from itertools import combinations
 from collections import defaultdict
 from string import ascii_lowercase
 
-
+# Mock Data for MVP
 sent1 = "Hello big and beautiful world "
 sent2 = "To be or not to be, This is the question"
 sent3 = "Be good to everyone, everywhere, everytime"
@@ -87,8 +87,6 @@ def replace_char(word):
     for index, char in enumerate(word):
         for i in ascii_lowercase:
             if word.replace(char, i, 1) in data.keys():
-                # score = 5 - index if index < 5 else 1
-                # score = (len(word) * 2) - score
                 score = (len(word) * 2) - (reduce_score(index) // 2)
                 return get_data_at_key(word.replace(char, i, 1)), score
     return [], 0
@@ -97,8 +95,6 @@ def replace_char(word):
 def delete_unnecessary_char(word):
     for index, char in enumerate(word):
         if word.replace(char, "", 1) in data.keys():
-            # score = (10 - 2 * index) if index < 4 else 2
-            # score = (len(word) * 2) - score
             score = (len(word) * 2) - reduce_score(index)
             return get_data_at_key(word.replace(char, "", 1)), score
     return [], 0
@@ -108,8 +104,6 @@ def add_missed_char(word):
     for index, char in enumerate(word):
         for i in ascii_lowercase:
             if word.replace(char, char + i, 1) in data.keys():
-                # score = (10 - 2 * index) if index < 4 else 2
-                # score = (len(word) * 2) - score
                 score = (len(word) * 2) - reduce_score(index)
                 return get_data_at_key(word.replace(char, char + i, 1)), score
     return [], 0
@@ -135,24 +129,20 @@ class AutoCompleteData:
         return self.score
 
 
-# if __name__ == '__main__':
-#     print("Loading the files and preparing the system...")
-#     init_data()
-#     # read_data("data.txt")
-#     text = input("The system is ready, Enter your text: ")
-#     while text != '#':
-#         result = get_best_k_completions(text)
-#         i = len(result)
-#         print(f"There are {i} suggestions:")
-#         for index, item in enumerate(result):
-#             print(f'{index + 1}. {item.get_completed_sentence()} ({item.get_source_text()} {item.get_offset()})')
-#         print(text)
-#         text = input()
+if __name__ == '__main__':
+    print("Loading the files and preparing the system...")
+    init_data()
+    # read_data("data.txt")
+    text = input("The system is ready, Enter your text: ")
+    while text != '#':
+        result = get_best_k_completions(text)
+        i = len(result)
+        print(f"There are {i} suggestions:")
+        for index, item in enumerate(result):
+            print(f'{index + 1}. {item.get_completed_sentence()} ({item.get_source_text()} {item.get_offset()})')
+        print(text)
+        text = input()
 
-# t = read_data("data.txt")
-# print_data(t)
-# print_data(data)
+
 init_data()
-a = AutoCompleteData("ello")
-print(a.completed_sentence)
-# print(find_complete_sentence("hello"))
+# a = AutoCompleteData("hello")
