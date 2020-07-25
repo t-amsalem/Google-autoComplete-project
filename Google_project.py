41d9c82ab8dc3425e5b976cb6071ad5b5f108232
Your Name <you@example.com>
2020-07-25 23:32:52 +0300

    give user the option to stop typing by the character #

--- Google_project.py
+++ Google_project.py
@@ -138,10 +138,16 @@ if __name__ == '__main__':
     read_file()
     init_data()
     text = input("The system is ready, Enter your text: ")
-    while text != '#':
+    while True:
+        if not text or "#" == text[-1]:
+            print("Enter your text:")
+            text = input()
         result = get_best_k_completions(text)
         i = len(result)
         print(f"There are {i} suggestions:")
         for index, item in enumerate(result):
             print(f'{index + 1}. {item.get_completed_sentence()} ({item.get_source_text()} {item.get_offset()})')
-        text = input(text)
+        print(text, end="")
+        text += input()
+
+
